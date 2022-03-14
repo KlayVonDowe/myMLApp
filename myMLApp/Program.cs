3a0c83ee6633978a12b731ca06f2fb948347fa1c
KlayVonDowe <70662432+KlayVonDowe@users.noreply.github.com>
2022-03-14 09:02:53 -0500

    Add project files.

---
+++ myMLApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using myMLApp;
+using MyMLApp;
+
+namespace myMLApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sampleData = new SentimentModel.ModelInput()
+            {
+                Col0 = "This restraunt was wonderful."
+            };
+
+            var result = SentimentModel.Predict(sampleData);
+
+            string sentiment = result.Prediction == 1 ? "Positive" : "Negative";
+            Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
+        }
+    }
+}
